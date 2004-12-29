0a7137772050e7bf96d0b7e1c9adfa3af8bd1d97
Remco Boerma <none@none>
2004-12-29 21:06:35 +0000

    converted \r\n to \n in all the .conf, .txt and .py files. '''use tools/linefeed.py before committing something''' No other change has been made.

---
+++ tools/linefeed.py
@@ -0,0 +1,33 @@
+#! /usr/bin/env python
+"Replace CRLF with LF cherrypy files.  Print names of changed files."
+
+import sys, os
+import glob, os.path
+import re
+
+def main():
+    scriptdir = os.path.split(sys.argv[0])[0]
+    basepath = os.path.split(scriptdir)[0]
+    filelist = []
+    for directory in ['/tools','/cherrypy','/cherrypy/lib/filter','/',
+                      '/cherrypy/lib','/cherrypy/test','/cherrypy/tutorial']:
+        filelist.extend(glob.glob(basepath+directory+'/*.py'))
+        filelist.extend(glob.glob(basepath+directory+'/*.conf'))
+        filelist.extend(glob.glob(basepath+directory+'/*.txt'))
+    for filename in filelist:
+        if os.path.isdir(filename):
+            print filename, "Directory!"
+            continue
+        data = open(filename, "rb").read()
+        if '\0' in data:
+            print filename, "Binary!"
+            continue
+        newdata = data.replace('\r\n','\n')
+        if newdata != data:
+            print filename
+            f = open(filename, "wb")
+            f.write(newdata)
+            f.close()
+
+if __name__ == '__main__':
+    main()
