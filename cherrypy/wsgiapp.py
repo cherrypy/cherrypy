a6d328c6ab58b7f2b0cfe046953bd50e76593aad
Peter Hunt <none@none>
2005-05-20 00:34:15 +0000

    fix for ticket #146. thanks, fumanchu!

--- cherrypy/wsgiapp.py
+++ cherrypy/wsgiapp.py
@@ -30,144 +30,183 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 WSGI interface for CherryPy
 """
 
-import StringIO, Cookie, time
+import StringIO
+import thread
 from cherrypy import cpg, _cphttptools, _cpserver
 
 def init(*a, **kw):
     kw['initOnly'] = 1
     _cpserver.start(*a, **kw)
 
-def wsgiApp(environ, start_response):
-    cpg.request.method = environ['REQUEST_METHOD']
+
+def requestLine(environ):
     # Rebuild first line of the request
-    pathInfo = environ['PATH_INFO']
+    resource = environ.get('SCRIPT_NAME', '') + environ.get('PATH_INFO', '')
     qString = environ.get('QUERY_STRING')
     if qString:
-        pathInfo += '?' + qString
-    firstLine = '%s %s %s' % (
-        environ['REQUEST_METHOD'],
-        pathInfo or '/',
-        environ['SERVER_PROTOCOL']
-    )
-    _cphttptools.parseFirstLine(firstLine)
+        resource += '?' + qString
+    return ('%s %s %s' % (environ['REQUEST_METHOD'],
+                          resource or '/',
+                          environ['SERVER_PROTOCOL']
+                          ))
 
-    # Initialize variables
-    now = time.time()
-    year, month, day, hh, mm, ss, wd, y, z = time.gmtime(now)
-    date = "%s, %02d %3s %4d %02d:%02d:%02d GMT" % (_cphttptools.weekdayname[wd], day, _cphttptools.monthname[month], year, hh, mm, ss)
-    cpg.request.headerMap = {}
-    cpg.request.simpleCookie = Cookie.SimpleCookie()
-    cpg.response.simpleCookie = Cookie.SimpleCookie()
 
-    # Rebuild headerMap
-    for cgiName, headerName in [
-        ('HTTP_HOST', 'Host'),
-        ('HTTP_USER_AGENT', 'User-Agent'),
-        ('HTTP_CGI_AUTHORIZATION', 'Authorization'),
-        ('CONTENT_LENGTH', 'Content-Length'),
-        ('CONTENT_TYPE', 'Content-Type'),
-        ('HTTP_COOKIE', 'Cookie'),
-        ('REMOTE_HOST', 'Remote-Host'),
-        ('REMOTE_ADDR', 'Remote-Addr'),
-        ('HTTP_REFERER', 'Referer'),
-        ('HTTP_ACCEPT_ENCODING', 'Accept-Encoding'),
-    ]:
-        if cgiName in environ:
-            _cphttptools.insertIntoHeaderMap(headerName, environ[cgiName])
+headerNames = {'HTTP_HOST': 'Host',
+               'HTTP_USER_AGENT': 'User-Agent',
+               'HTTP_CGI_AUTHORIZATION': 'Authorization',
+               'CONTENT_LENGTH': 'Content-Length',
+               'CONTENT_TYPE': 'Content-Type',
+               'HTTP_COOKIE': 'Cookie',
+               'REMOTE_HOST': 'Remote-Host',
+               'REMOTE_ADDR': 'Remote-Addr',
+               'HTTP_REFERER': 'Referer',
+               'HTTP_ACCEPT_ENCODING': 'Accept-Encoding',
+               }
 
-    #  TODO: handle POST
-
-    # set up stuff similar to initRequest
-    cpg.response.headerMap = {
-        "protocolVersion": cpg.configOption.protocolVersion,
-        "Status": "200 OK",
-        "Content-Type": "text/html",
-        "Server": "CherryPy/" + cpg.__version__,
-        "Date": date,
-        "Set-Cookie": [],
-        "Content-Length": 0
-    }
-    cpg.request.base = "http://" + cpg.request.headerMap['Host']
-    cpg.request.browserUrl = cpg.request.base + cpg.request.browserUrl
-    cpg.request.isStatic = False
-    cpg.request.parsePostData = True
-    cpg.request.rfile = environ["wsgi.input"]
-    cpg.request.objectPath = None 
-    if 'Cookie' in cpg.request.headerMap:
-        cpg.request.simpleCookie.load(cpg.request.headerMap['Cookie'])
-
-    cpg.response.simpleCookie = Cookie.SimpleCookie()
-    cpg.response.sendResponse = 1
+class wsgiHeaders(object):
     
-    if cpg.request.method == 'POST' and cpg.request.parsePostData:
-        _cphttptools.parsePostData(cpg.request.rfile)
-
-    # Execute request
-    wfile = StringIO.StringIO()
-    cpg.response.wfile = wfile
-    _cphttptools.handleRequest(wfile)
-    response = wfile.getvalue()
-
-
-    # Extract header from response
-    headerLines = []
-    i = 0
-    while 1:
-        j = response.find('\n', i)
-        line = response[i:j]
-        if line[-1] == '\r':
-            line = line[:-1]
-        headerLines.append(line)
-        i = j+1
-        if not line:
-            break
-    response = response[i:]
-
-    status = headerLines[0]
-    # Remove "HTTP/1.0" at the beginning of status
-    i = status.find(' ')
-    status = status[i+1:]
-
-    responseHeaders = []
-    for line in headerLines[1:]:
-        i = line.find(':')
-        header = line[:i]
-        value = line[i+1:].lstrip()
-        responseHeaders.append((header,value))
-
-    start_response(status, responseHeaders)
-
-    return [response]
-
-if __name__ == '__main__':
-    from cherrypy import cpg, wsgiapp
-    class Root:
-        def index(self, name = "world"):
-            count = cpg.request.sessionMap.get('count', 0) + 1
-            cpg.request.sessionMap['count'] = count
-            return """
-                <html><body>
-                Hello, %s, count is %s:
-                <form action="/post" method="post">
-                    Post some data: <input name=myData type=text"> <input type=submit>
-                </form>
-            """ % (name, count)
-        index.exposed = True
-        def post(self, myData):
-            return "myData: " + myData
-        post.exposed = True
-    cpg.root = Root()
+    def __init__(self, environ):
+        self.data = []
+        for cgiName in environ:
+            translatedHeader = headerNames.get(cgiName.upper())
+            if translatedHeader:
+                self.data.append((translatedHeader, environ[cgiName]))
     
-    import sys
-    # This uses the WSGI HTTP server from PEAK.wsgiref
-    # sys.path.append(r"C:\Tmp\PEAK\src")
-    from wsgiref.simple_server import WSGIServer, WSGIRequestHandler
-     # Read the CherryPy config file and initialize some variables
-    wsgiapp.init(configMap = {'socketPort': 8000, 'sessionStorageType': 'ram'})
-    server_address = ("", 8000)
-    httpd = WSGIServer(server_address, WSGIRequestHandler)
-    httpd.set_app(wsgiapp.wsgiApp)
-    sa = httpd.socket.getsockname()
-    print "Serving HTTP on", sa[0], "port", sa[1], "..."
-    httpd.serve_forever()
+    def items(self):
+        return self.data
+    
+    def getallmatchingheaders(self, name):
+        name = name.lower()
+        for key, value in self.data:
+            if key.lower() == name:
+                yield "%s: %s" % (key, value)
 
+
+class WSGIWriter(object):
+    """wfile proxy for WSGI, using the 'write callable' hack."""
+    
+    def __init__(self, start_response):
+        self.start_response = start_response
+        self.buf = StringIO.StringIO()
+        self.write_body = None
+        self.closed = False
+    
+    def flush(self):
+        if self.closed:
+            raise ValueError("The output stream has been closed.")
+    
+    def close(self):
+        # Note that we do not call .close() on the write_body callable.
+        # That is specifically denied by PEP 333.
+        self.closed = True
+    
+    def clean_status(self, line):
+        status = line[len("HTTP/1.0 "):]
+        
+        # WSGI requires a Reason-phrase. Provide one if missing.
+        try:
+            code, reason = status.split(" ", 1)
+            reason = reason.strip()
+        except ValueError:
+            code, reason = status, ""
+        code = code.strip()
+        assert code.isdigit()
+        if not reason:
+            reason = 'CherryPy'
+        status = " ".join((code, reason))
+        return status
+    
+    def write(self, data):
+        if self.closed:
+            raise ValueError("The output stream has been closed.")
+        
+        # WSGI requires all data to be of type "str". This coercion should
+        # not take any time at all if data is already of type "str".
+        # If it's unicode, it could be a big performance hit (x ~500).
+        data = str(data)
+        
+        if self.write_body is None:
+            # We haven't finished the headers yet.
+            self.buf.write(data)
+            buffer = self.buf.getvalue()
+            end_of_headers = buffer.find("\r\n\r\n")
+            if end_of_headers != -1:
+                # Reached the end of headers.
+                # Separate, process, and send the headers.
+                headers = buffer[:end_of_headers]
+                buffer = buffer[end_of_headers + 4:]
+                
+                headers = headers.split("\r\n")
+                status = self.clean_status(headers.pop(0))
+                
+                # Notice we're not handling multiline headers.
+                # AFAICT, CherryPy doesn't write any, and PEP 333
+                # specifically denies them.
+                headers = [tuple(line.split(": ", 1)) for line in headers]
+                
+                # Use the "write callable" hack described in PEP 333.
+                # If CherryPy ever moves to "generators all the way down",
+                # then we can avoid this hack.
+                self.write_body = self.start_response(status, headers)
+                if buffer:
+                    self.write_body(buffer)
+        else:
+            self.write_body(data)
+
+
+seen_threads = {}
+
+def wsgiApp(environ, start_response):
+    
+    try:
+        threadID = thread.get_ident()
+        if threadID not in seen_threads:
+            # Call the functions from cpg.server.onStartThreadList
+            seen_threads[threadID] = None
+            for func in cpg.server.onStartThreadList:
+                func(len(seen_threads))
+        
+        cpg.request.method = environ['REQUEST_METHOD']
+        
+        _cphttptools.doRequest(environ['REMOTE_ADDR'],
+                               environ['REMOTE_ADDR'],
+                               requestLine(environ),
+                               wsgiHeaders(environ),
+                               environ['wsgi.input'],
+                               WSGIWriter(start_response),
+                               )
+    except:
+        import sys, traceback, _cputil
+        exc_info = sys.exc_info()
+        tb = traceback.format_exception(*exc_info)
+        _cputil.getSpecialFunction('_cpLogMessage')("".join(tb))
+        # TODO: override _cpdefaults._cpOnError() to call us back
+        # in some way (maybe set a flag in the WSGIWriter), so that
+        # we can still call start_response with an exc_info argument
+        # even if doRequest traps its own errors.
+        headers = [('Content-Type', 'text/plain'),
+                   ('Content-Length', '0')]
+        start_response("500 Internal Server Error", headers, exc_info)
+    
+    # We use the "write callable" hack, but we still must return an iterable.
+    return []
+
+
+def start():
+    # Call the functions from cpg.server.onStartServerList
+    for func in cpg.server.onStartServerList:
+        func()
+
+# "Start the server" on module import.
+start()
+
+def stop():
+    # Call the functions from cpg.server.onStopThreadList
+    for func in cpg.server.onStopThreadList:
+        func()
+    
+    seen_threads.clear()
+    
+    # Call the functions from cpg.server.onStopServerList
+    for func in cpg.server.onStopServerList:
+        func()
