0b51af18839443e5505ec9d39dc0732cc7f68a3f
Robert Brewer <fumanchu@aminus.org>
2005-09-02 08:34:08 +0000

    Updated lib\defaultformmask.py to smart attribute quoting and XHTML.

--- cherrypy/lib/defaultformmask.py
+++ cherrypy/lib/defaultformmask.py
@@ -30,68 +30,104 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 Default mask for the form.py module
 """
 
+from xml.sax.saxutils import quoteattr as q
+
+
+def selected(value):
+    """If value is True, return a valid XHTML 'selected' attribute, else ''."""
+    if value:
+        return ' selected="selected" '
+    return ''
+
+def checked(value):
+    """If value is True, return a valid XHTML 'checked' attribute, else ''."""
+    if value:
+        return ' checked="checked" '
+    return ''
+
+
 def defaultMask(field):
-    res="<tr><td valign=top>%s</td>"%field.label
-    if field.typ=='text':
-        res+='<td><input name="%s" type=text value="%s" size=%s></td>'%(field.name, field.currentValue, field.size)
-    elif field.typ=='forced':
-        res+='<td><input name="%s" type=hidden value="%s">%s</td>'%(field.name, field.currentValue, field.currentValue)
-    elif field.typ=='password':
-        res+='<td><input name="%s" type=password value="%s"></td>'%(field.name, field.currentValue)
-    elif field.typ=='select':
-        res+='<td><select name="%s">'%field.name
+    
+    res = ["<tr>",
+           "<td valign='top'>%s</td>" % field.label]
+    
+    if field.typ == 'text':
+        res.append('<td><input name=%s type="text" value=%s size=%s /></td>'
+                   % (q(field.name), q(field.currentValue), q(field.size)))
+    elif field.typ == 'forced':
+        res.append('<td><input name=%s type="hidden" value=%s />%s</td>'
+                   % (q(field.name), q(field.currentValue), field.currentValue))
+    elif field.typ == 'password':
+        res.append('<td><input name=%s type="password" value=%s /></td>'
+                   % (q(field.name), q(field.currentValue)))
+    elif field.typ == 'select':
+        res.append('<td><select name=%s>' % q(field.name))
         for option in field.optionList:
-            if type(option)==type(()):
-                optionId, optionLabel=option
-                if optionId==field.currentValue or str(optionId)==field.currentValue: res+="<option selected value=%s>%s</option>"%(optionId, optionLabel)
-                else: res+="<option value=%s>%s</option>"%(optionId, optionLabel)
+            if isinstance(option, tuple):
+                id, option = option
+                sel = selected(field.currentValue in (id, str(id)))
+                value = " value=%s" % q(id)
             else:
-                if option==field.currentValue: res+="<option selected>%s</option>"%option
-                else: res+="<option>%s</option>"%option
-        res+='</select></td>'
-    elif field.typ=='textarea':
-        # Size is colsxrows
-        if field.size==15: size="15x15"
-        else: size=field.size
-        cols, rows=size.split('x')
-        res+='<td><textarea name="%s" rows="%s" cols="%s">%s</textarea></td>'%(field.name, rows, cols, field.currentValue)
-    elif field.typ=='submit':
-        res+='<td><input type=submit value="%s"></td>'%field.name
-    elif field.typ=='hidden':
-        if type(field.currentValue)==type([]): currentValue=field.currentValue
-        else: currentValue=[field.currentValue]
-        res=""
-        for value in currentValue:
-            res+='<input name="%s" type=hidden value="%s">'%(field.name, value)
-        return res
-    elif field.typ=='checkbox' or field.typ=='radio':
-        res+='<td>'
-        # print "##### currentValue:", field.currentValue # TBC
+                sel = selected(option == field.currentValue)
+                value = ""
+            res.append("    <option%s%s>%s</option>" % (sel, value, option))
+        res.append('</select></td>')
+    elif field.typ == 'textarea':
+        # Size is cols x rows
+        if field.size == 15:
+            size = "15x15"
+        else:
+            size = field.size
+        cols, rows = size.split('x')
+        res.append('<td><textarea name=%s rows="%s" cols="%s">%s</textarea></td>'
+                   % (q(field.name), rows, cols, field.currentValue))
+    elif field.typ == 'submit':
+        res.append('<td><input type="submit" value=%s /></td>' % q(field.name))
+    elif field.typ == 'hidden':
+        if isinstance(field.currentValue, list):
+            vals = field.currentValue
+        else:
+            vals = [field.currentValue]
+        i = '<input name=%s type="hidden" value=%%s />' % q(field.name)
+        return [i % q(v) for v in vals]
+    elif field.typ in ('checkbox', 'radio'):
+        res.append('<td>')
         for option in field.optionList:
-            if type(option)==type(()): optionValue, optionLabel=option
-            else: optionValue, optionLabel=option, option
-            res+='<input type="%s" name="%s" value="%s"'%(field.typ, field.name, optionValue)
-            if type(field.currentValue)==type([]):
-                if optionValue in field.currentValue: res+=' checked'
+            if isinstance(option, tuple):
+                val, label = option
             else:
-                if optionValue==field.currentValue: res+=' checked'
-            res+='>&nbsp;&nbsp;%s<br>'%optionLabel
-        res+='</td>'
+                val, label = option, option
+            
+            if isinstance(field.currentValue, list):
+                c = checked(val in field.currentValue)
+            else:
+                c = checked(val == field.currentValue)
+            
+            res.append('<input type="%s" name=%s value=%s %s />&nbsp;&nbsp;%s<br />'
+                       % (field.typ, q(field.name), q(val), c, label))
+        res.append('</td>')
+    
     if field.errorMessage:
-        res+="<td><font color=red>%s</font></td>"%field.errorMessage
+        res.append("<td><font color='red'>%s</font></td>" % field.errorMessage)
     else:
-        res+="<td>&nbsp;</td>"
-    return res+"</tr>"
+        res.append("<td>&nbsp;</td>")
+    
+    res.append("</tr>")
+    return "\n".join(res)
+
 def hiddenMask(field):
-        if type(field.currentValue)==type([]): currentValue=field.currentValue
-        else: currentValue=[field.currentValue]
-        res=""
-        for value in currentValue:
-            res+='<input name="%s" type=hidden value="%s">'%(field.name, value)
-        return res
+    if isinstance(field.currentValue, list):
+        currentValue = field.currentValue
+    else:
+        currentValue = [field.currentValue]
+    return "\n".join(['<input name=%s type="hidden" value=%s />' %
+                      (q(field.name), q(value)) for value in currentValue])
+
 def defaultHeader(label):
     return "<table>"
+
 def defaultFooter(label):
     return "</table>"
+
 def echoMask(label):
     return label
