aa88403f18a9b83074f83c910fe4d521f4338a3e
Remi Delon <rdelon@cherrypy.org>
2005-10-16 20:22:45 +0000

    Made default error message fully XHTML complient

--- cherrypy/_cputil.py
+++ cherrypy/_cputil.py
@@ -156,7 +156,7 @@ _HTTPErrorTemplate = '''<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
   "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html>
 <head>
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
     <title>%(status)s</title>
     <style type="text/css">
     #poweredBy {
