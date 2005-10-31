ffac61c19ce192ab2d13f464e33769c562e14498
Robert Brewer <fumanchu@aminus.org>
2005-10-31 18:46:05 +0000

    Fix for #374 (reduce license boilerplate to a single LICENSE.txt file).

--- cherrypy/test/helper.py
+++ cherrypy/test/helper.py
@@ -1,48 +1,21 @@
-"""
-Copyright (c) 2004, CherryPy Team (team@cherrypy.org)
-All rights reserved.
+"""A library of helper functions for the CherryPy test suite.
 
-Redistribution and use in source and binary forms, with or without modification, 
-are permitted provided that the following conditions are met:
+The actual script that runs the entire CP test suite is called
+"test.py" (in this folder); test.py calls this module as a library.
 
-    * Redistributions of source code must retain the above copyright notice, 
-      this list of conditions and the following disclaimer.
-    * Redistributions in binary form must reproduce the above copyright notice, 
-      this list of conditions and the following disclaimer in the documentation 
-      and/or other materials provided with the distribution.
-    * Neither the name of the CherryPy Team nor the names of its contributors 
-      may be used to endorse or promote products derived from this software 
-      without specific prior written permission.
-
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND 
-ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED 
-WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE 
-DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE 
-FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL 
-DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR 
-SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER 
-CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, 
-OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE 
-OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+Usage:
+  Each individual test_*.py module imports this module (helper),
+  usually to make an instance of CPWebCase, and then call testmain().
+  
+  The CP test suite script (test.py) imports this module and calls
+  run_test_suite, possibly more than once. CP applications may also
+  import test.py (to use TestHarness), which then calls helper.py.
 """
 
-# This is a library of helper functions for the CherryPy test suite.
-# The actual script that runs the entire CP test suite is called
-# "test.py" (in this folder); test.py calls this module as a library.
-# 
 # GREAT CARE has been taken to separate this module from test.py,
 # because different consumers of each have mutually-exclusive import
 # requirements. So don't go moving functions from here into test.py,
 # or vice-versa, unless you *really* know what you're doing.
-# 
-# Usage:
-#   Each individual test_*.py module imports this module (helper),
-#   usually to make an instance of CPWebCase, and then call testmain().
-#   
-#   The CP test suite script (test.py) imports this module and calls
-#   run_test_suite, possibly more than once. CP applications may also
-#   import test.py (to use TestHarness), which then calls helper.py.
-
 
 import os, os.path
 import re
