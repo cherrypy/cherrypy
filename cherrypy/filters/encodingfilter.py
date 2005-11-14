953dff37f4b61f119188f41a060866fb9ac98e0d
Robert Brewer <fumanchu@aminus.org>
2005-11-14 02:54:18 +0000

    Improved EncodingFilter. As before, if "encodingFilter.encoding" is provided, that encoding is forced. But if not provided, the filter now inspects the Accept-Charset request header. Use an "encodingFilter.defaultEncoding" entry (default = utf-8) to handle "Accept-Charset: *".

--- cherrypy/filters/encodingfilter.py
+++ cherrypy/filters/encodingfilter.py
@@ -1,26 +1,93 @@
 import cherrypy
 from basefilter import BaseFilter
 
+
 class EncodingFilter(BaseFilter):
     """Filter that automatically encodes the response."""
+    
+    def find_acceptable_charset(self):
+        conf = cherrypy.config.get
+        response = cherrypy.response
+        
+        stream = conf("streamResponse", False)
+        if stream:
+            # Use a generator wrapper, and just pray it works as the
+            # stream is being written out.
+            def encode_body(encoding):
+                def encoder(body):
+                    for line in body:
+                        yield line.encode(encoding)
+                response.body = encoder(response.body)
+                return True
+        else:
+            response.body = ''.join([chunk for chunk in response.body])
+            def encode_body(encoding):
+                try:
+                    response.body = response.body.encode(encoding)
+                except UnicodeError:
+                    # Try the next encoding
+                    return False
+                else:
+                    return True
+        
+        failmsg = "The response could not be encoded with %s"
+        
+        enc = conf('encodingFilter.encoding', None)
+        if enc is not None:
+            # If specified, force this encoding to be used, or fail.
+            if encode_body(enc):
+                return enc
+            else:
+                raise cherrypy.HTTPError(500, failmsg % enc)
+        
+        # Parse the Accept_Charset request header, and try to provide one
+        # of the requested charsets (in order of user preference).
+        default_enc = conf('encodingFilter.defaultEncoding', 'utf-8')
+        
+        encs = cherrypy.request.headerMap.elements('Accept-Charset')
+        if encs is None:
+            # Any character-set is acceptable.
+            if encode_body(default_enc):
+                return default_enc
+            else:
+                raise cherrypy.HTTPError(500, failmsg % default_enc)
+        else:
+            newbody = None
+            charsets = [enc.value.lower() for enc in encs]
+            if "*" not in charsets:
+                # If no "*" is present in an Accept-Charset field, then all
+                # character sets not explicitly mentioned get a quality
+                # value of 0, except for ISO-8859-1, which gets a quality
+                # value of 1 if not explicitly mentioned.
+                iso = 'iso-8859-1'
+                if iso not in charsets:
+                    if encode_body(iso):
+                        return iso
+            
+            for element in encs:
+                if element.qvalue > 0:
+                    if element.value == "*":
+                        # Matches any charset. Try our default.
+                        if encode_body(default_enc):
+                            return default_enc
+                    else:
+                        if encode_body(element.value):
+                            return element.value
+        
+        # No suitable encoding found.
+        raise cherrypy.HTTPError(406)
     
     def beforeFinalize(self):
         conf = cherrypy.config.get
         if not conf('encodingFilter.on', False):
             return
         
-        contentType = cherrypy.response.headerMap.get("Content-Type")
-        if contentType:
-            ctlist = contentType.split(';')[0]
-            if (ctlist in conf('encodingFilter.mimeTypeList', ['text/html'])):
-                enc = conf('encodingFilter.encoding', 'utf-8')
-                
-                # Add "charset=..." to response Content-Type header
-                if contentType and 'charset' not in contentType:
-                    cherrypy.response.headerMap["Content-Type"] += ";charset=%s" % enc
-                
-                # Return a generator that encodes the sequence
-                def encode_body(body):
-                    for line in body:
-                        yield line.encode(enc)
-                cherrypy.response.body = encode_body(cherrypy.response.body)
+        charset = self.find_acceptable_charset()
+        
+        # Set "charset=..." param on response Content-Type header
+        ct = cherrypy.response.headerMap.elements("Content-Type")
+        if ct is not None:
+            ct = ct[0]
+            ct.params['charset'] = charset
+            cherrypy.response.headerMap["Content-Type"] = str(ct)
+
--- cherrypy/test/test_decodingencoding_filter.py
+++ cherrypy/test/test_decodingencoding_filter.py
@@ -9,6 +9,10 @@ class Root:
         assert param == europoundUnicode
         yield europoundUnicode
     index.exposed = True
+    
+    def mao_zedong(self):
+        return u"\u6bdb\u6cfd\u4e1c: Sing, Little Birdie?"
+    mao_zedong.exposed = True
 
 cherrypy.root = Root()
 cherrypy.config.update({
@@ -21,13 +25,41 @@ cherrypy.config.update({
 
 import helper
 
-europoundUtf8 = europoundUnicode.encode('utf-8')
 
 class DecodingEncodingFilterTest(helper.CPWebCase):
     
     def testDecodingEncodingFilter(self):
+        europoundUtf8 = europoundUnicode.encode('utf-8')
         self.getPage('/?param=%s' % europoundUtf8)
         self.assertBody(europoundUtf8)
+        
+        # Default encoding should be utf-8
+        self.getPage('/mao_zedong')
+        self.assertBody("\xe6\xaf\x9b\xe6\xb3\xbd\xe4\xb8\x9c: "
+                        "Sing, Little Birdie?")
+        
+        # Ask for utf-16.
+        sing16 = ('\xff\xfe\xdbk\xfdl\x1cN:\x00 \x00S\x00i\x00n\x00g\x00,\x00 '
+                  '\x00L\x00i\x00t\x00t\x00l\x00e\x00 '
+                  '\x00B\x00i\x00r\x00d\x00i\x00e\x00?\x00')
+        self.getPage('/mao_zedong', [('Accept-Charset', 'utf-16')])
+        self.assertBody(sing16)
+        
+        # Ask for multiple encodings. ISO-8859-1 should fail, and utf-16
+        # should be produced.
+        self.getPage('/mao_zedong', [('Accept-Charset',
+                                      'iso-8859-1;q=1, utf-16;q=0.5')])
+        self.assertBody(sing16)
+        
+        # The "*" value should default to our defaultEncoding, utf-8
+        self.getPage('/mao_zedong', [('Accept-Charset', '*;q=1, utf-7;q=.2')])
+        self.assertBody("\xe6\xaf\x9b\xe6\xb3\xbd\xe4\xb8\x9c: "
+                        "Sing, Little Birdie?")
+        
+        # Only allow iso-8859-1, which should fail and raise 406.
+        self.getPage('/mao_zedong', [('Accept-Charset', 'iso-8859-1, *;q=0')])
+        self.assertStatus("406 Not Acceptable")
+        self.assertErrorPage(406)
 
 
 if __name__ == "__main__":
