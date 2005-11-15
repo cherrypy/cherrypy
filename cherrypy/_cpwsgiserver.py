3ece0e7ae6b9f2e62b6d755ce0ab357329f90316
Robert Brewer <fumanchu@aminus.org>
2005-11-15 15:48:08 +0000

    Fix for #389. Yet another socket error to ignore.

--- cherrypy/_cpwsgiserver.py
+++ cherrypy/_cpwsgiserver.py
@@ -217,7 +217,7 @@ class WorkerThread(threading.Thread):
                                 request.write(line)
                     except socket.error, e:
                         errno = e.args[0]
-                        if errno in (32, 104, 10053, 10054):
+                        if errno in (32, 104, 113, 10053, 10054):
                             # Client probably closed the connection before the
                             # response was sent.
                             pass
