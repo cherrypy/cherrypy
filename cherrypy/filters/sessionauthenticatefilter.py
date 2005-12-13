a7675742a79d420968c033b9cafac5ea38371f66
Remi Delon <rdelon@cherrypy.org>
2005-12-13 09:20:15 +0000

    More PEP 8 in session_authenticatr_filter

--- cherrypy/filters/sessionauthenticatefilter.py
+++ cherrypy/filters/sessionauthenticatefilter.py
@@ -2,7 +2,7 @@ import cherrypy
 from basefilter import BaseFilter
 
 
-def defaultLoginScreen(from_page, login = '', errorMsg = ''):
+def default_login_screen(from_page, login = '', error_msg = ''):
     return """
     <html><body>
         Message: %s
@@ -13,10 +13,10 @@ def defaultLoginScreen(from_page, login = '', errorMsg = ''):
             <input type="submit"/>
         </form>
     </body></html>
-    """ % (errorMsg, login, from_page)
+    """ % (error_msg, login, from_page)
 
-def defaultCheckLoginAndPassword(login, password):
-    # Dummy checkLoginAndPassword function
+def default_check_login_and_password(login, password):
+    # Dummy check_login_and_password function
     if login != 'login' or password != 'password':
         return u'Wrong login/password'
 
@@ -31,16 +31,16 @@ class SessionAuthenticateFilter(BaseFilter):
               or conf('static_filter.on', False)):
             return
         
-        checkLoginAndPassword = cherrypy.config.get('session_authenticate_filter.check_login_and_password', defaultCheckLoginAndPassword)
-        loginScreen = cherrypy.config.get('session_authenticate_filter.login_screen', defaultLoginScreen)
-        notLoggedIn = cherrypy.config.get('session_authenticate_filter.not_logged_in')
-        loadUserByUsername = cherrypy.config.get('session_authenticate_filter.load_user_by_username')
-        sessionKey = cherrypy.config.get('session_authenticate_filter.session_key', 'username')
+        check_login_and_password = cherrypy.config.get('session_authenticate_filter.check_login_and_password', default_check_login_and_password)
+        login_screen = cherrypy.config.get('session_authenticate_filter.login_screen', default_login_screen)
+        not_logged_in = cherrypy.config.get('session_authenticate_filter.not_logged_in')
+        load_user_by_username = cherrypy.config.get('session_authenticate_filter.load_user_by_username')
+        session_key = cherrypy.config.get('session_authenticate_filter.session_key', 'username')
 
-        if cherrypy.request.path.endswith('loginScreen'):
+        if cherrypy.request.path.endswith('login_screen'):
             return
         elif cherrypy.request.path.endswith('do_logout'):
-            cherrypy.session[sessionKey] = None
+            cherrypy.session[session_key] = None
             cherrypy.request.user = None
             from_page = cherrypy.request.params.get('from_page', '..')
             raise cherrypy.HTTPRedirect(from_page)
@@ -48,29 +48,29 @@ class SessionAuthenticateFilter(BaseFilter):
             from_page = cherrypy.request.params.get('from_page', '..')
             login = cherrypy.request.params['login']
             password = cherrypy.request.params['password']
-            errorMsg = checkLoginAndPassword(login, password)
-            if errorMsg:
-                cherrypy.response.body = loginScreen(from_page, login = login, errorMsg = errorMsg)
+            error_msg = check_login_and_password(login, password)
+            if error_msg:
+                cherrypy.response.body = login_screen(from_page, login = login, error_msg = error_msg)
                 cherrypy.request.executeMain = False
             else:
-                cherrypy.session[sessionKey] = login
+                cherrypy.session[session_key] = login
                 if not from_page:
                     from_page = '/'
                 raise cherrypy.HTTPRedirect(from_page)
             return
 
         # Check if user is logged in
-        if (not cherrypy.session.get(sessionKey)) and notLoggedIn:
-            # Call notLoggedIn so that applications where anynymous user
+        if (not cherrypy.session.get(session_key)) and not_logged_in:
+            # Call not_logged_in so that applications where anynymous user
             #   is OK can handle it
-            notLoggedIn()
-        if not cherrypy.session.get(sessionKey):
-            cherrypy.response.body = loginScreen(cherrypy.request.browser_url)
+            not_logged_in()
+        if not cherrypy.session.get(session_key):
+            cherrypy.response.body = login_screen(cherrypy.request.browser_url)
             cherrypy.request.executeMain = False
             return
         
         # Everything is OK: user is logged in
-        if loadUserByUsername:
-            username = cherrypy.session[sessionKey]
-            cherrypy.request.user = loadUserByUsername(username)
-            cherrypy.thread_data.user = loadUserByUsername(username)
+        if load_user_by_username:
+            username = cherrypy.session[session_key]
+            cherrypy.request.user = load_user_by_username(username)
+            cherrypy.thread_data.user = load_user_by_username(username)
