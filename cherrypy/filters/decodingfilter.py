faa7d89cecddad4845314bc62d755cad12a513dc
Robert Brewer <fumanchu@aminus.org>
2005-12-17 05:44:25 +0000

    New decoding_filter.default_encoding config entry, and better guessing/retry of form encoding.

--- cherrypy/filters/decodingfilter.py
+++ cherrypy/filters/decodingfilter.py
@@ -5,19 +5,50 @@ class DecodingFilter(BaseFilter):
     """Automatically decodes request parameters (except uploads)."""
     
     def before_main(self):
-        if not cherrypy.config.get('decoding_filter.on', False):
+        conf = cherrypy.config.get
+        
+        if not conf('decoding_filter.on', False):
             return
         
-        enc = cherrypy.config.get('decoding_filter.encoding', 'utf-8')
+        enc = conf('decoding_filter.encoding', None)
+        if not enc:
+            ct = cherrypy.request.headers.elements("Content-Type")
+            if ct is not None:
+                ct = ct[0]
+                enc = ct.params.get("charset", None)
+                if (not enc) and ct.value.lower().startswith("text/"):
+                    # http://www.w3.org/Protocols/rfc2616/rfc2616-sec3.html#sec3.7.1
+                    # When no explicit charset parameter is provided by the
+                    # sender, media subtypes of the "text" type are defined
+                    # to have a default charset value of "ISO-8859-1" when
+                    # received via HTTP.
+                    enc = "ISO-8859-1"
+            
+            if not enc:
+                enc = conf('decoding_filter.default_encoding', "utf-8")
+        
+        try:
+            self.decode(enc)
+        except UnicodeDecodeError:
+            # IE and Firefox don't supply a charset when submitting form
+            # params with a CT of application/x-www-form-urlencoded.
+            # So after all our guessing, it could *still* be wrong.
+            # Start over with ISO-8859-1, since that seems to be preferred.
+            self.decode("ISO-8859-1")
+    
+    def decode(self, enc):
+        decodedParams = {}
         for key, value in cherrypy.request.params.items():
             if hasattr(value, 'file'):
                 # This is a file being uploaded: skip it
-                continue
+                decodedParams[key] = value
             if isinstance(value, list):
                 # value is a list: decode each element
-                newValue = [v.decode(enc) for v in value]
+                decodedParams[key] = [v.decode(enc) for v in value]
             else:
                 # value is a regular string: decode it
-                newValue = value.decode(enc)
-            cherrypy.request.params[key] = newValue
+                decodedParams[key] = value.decode(enc)
+        
+        # Decode all or nothing, so we can try again on error.
+        cherrypy.request.params = decodedParams
 
