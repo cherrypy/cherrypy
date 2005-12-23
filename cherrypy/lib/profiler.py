222767993ab13f80fab7eeda0c28c07c68c2fc29
Robert Brewer <fumanchu@aminus.org>
2005-12-23 22:24:28 +0000

    Oops. Debian is missing the pstats module too.

--- cherrypy/lib/profiler.py
+++ cherrypy/lib/profiler.py
@@ -38,13 +38,14 @@ def new_func_strip_path(func_name):
     if filename.endswith("__init__.py"):
         return os.path.basename(filename[:-12]) + filename[-12:], line, name
     return os.path.basename(filename), line, name
-import pstats
-pstats.func_strip_path = new_func_strip_path
 
 try:
     import profile
+    import pstats
+    pstats.func_strip_path = new_func_strip_path
 except ImportError:
     profile = None
+    pstats = None
     import warnings
     msg = ("Your installation of Python doesn't have a profile module. "
            "If you're on Debian, you can apt-get python2.4-profiler from "
