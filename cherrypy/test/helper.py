6f3234d8c6909cfe00e2a87f7ef6b18ebd0361da
Robert Brewer <fumanchu@aminus.org>
2005-12-29 18:28:28 +0000

    Set WebCase.PORT in testmain.

--- cherrypy/test/helper.py
+++ cherrypy/test/helper.py
@@ -235,6 +235,7 @@ def testmain(server=None, conf=None):
 
 def _test_main_thread():
     try:
+        webtest.WebCase.PORT = cherrypy.config.get('server.socket_port')
         webtest.main()
     finally:
         thread.interrupt_main()
