5281fe974d091fdcfa3b5bfc0beb998e1969a9de
Robert Brewer <fumanchu@aminus.org>
2006-01-07 20:02:58 +0000

    Fix for #434 (importing from cherrypy.lib.filter broken in trunk).

--- cherrypy/lib/filter/__init__.py
+++ cherrypy/lib/filter/__init__.py
@@ -2,3 +2,16 @@ import warnings
 warnings.warn("cherrypy.lib.filter has been superseded by cherrypy.filters and will be removed in CP 2.3")
 
 from cherrypy.filters import *
+
+import sys
+builtin_filters = ("basefilter", "baseurlfilter", "cachefilter",
+                   "decodingfilter", "encodingfilter", "gzipfilter",
+                   "logdebuginfofilter", "nsgmlsfilter",
+                   "responseheadersfilter", "sessionauthenticatefilter",
+                   "sessionfilter", "staticfilter", "tidyfilter",
+                   "virtualhostfilter", "xmlrpcfilter")
+for name in builtin_filters:
+    newlocation = "cherrypy.filters." + name
+    m = __import__(newlocation, globals(), locals(), [''])
+    sys.modules["cherrypy.lib.filter." + name] = m
+    globals()[name] = m
