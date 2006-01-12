f3d1766f9c53117cd30e3fe9124df9b22bcd26a1
Remi Delon <rdelon@cherrypy.org>
2006-01-12 12:45:24 +0000

    Better formatting of tracebacks: the page handler is now included at the top. That way the error it still a string and not a tuple so an error message can be on multiple lines and it gets displayed fine in the browser

--- cherrypy/_cputil.py
+++ cherrypy/_cputil.py
@@ -323,7 +323,12 @@ def formatExc(exc=None):
     
     if exc == (None, None, None):
         return ""
-    return "".join(traceback.format_exception(*exc))
+    args = exc[1].args
+    page_handler = args[-1]
+    page_handler_str = 'Page handler: %s\n' % repr(page_handler)
+    args = args[:-1]
+    exc[1].args = args
+    return page_handler_str + "".join(traceback.format_exception(*exc))
 
 def bareError(extrabody=None):
     """Produce status, headers, body for a critical error.
