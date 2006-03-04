bf8bedfd0ae67812d5c80e19ac33166d82800950
Robert Brewer <fumanchu@aminus.org>
2006-03-04 00:26:01 +0000

    Fix for #476 (BaseURLFilter doesn't use socket_port).

--- cherrypy/filters/baseurlfilter.py
+++ cherrypy/filters/baseurlfilter.py
@@ -13,7 +13,14 @@ class BaseUrlFilter(BaseFilter):
             return
         
         request = cherrypy.request
-        newBaseUrl = cherrypy.config.get('base_url_filter.base_url', 'http://localhost')
+        
+        port = str(cherrypy.config.get('server.socket_port', '80'))
+        if port == "80":
+            defaultUrl = 'http://localhost'
+        else:
+            defaultUrl = 'http://localhost:%s' % port
+        newBaseUrl = cherrypy.config.get('base_url_filter.base_url', defaultUrl)
+        
         if cherrypy.config.get('base_url_filter.use_x_forwarded_host', True):
             newBaseUrl = request.headers.get("X-Forwarded-Host", newBaseUrl)
         
