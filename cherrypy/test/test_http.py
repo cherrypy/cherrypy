eea03a9cbfaa9c30cb8075e0c02adff8a8c3a5d5
Robert Brewer <fumanchu@aminus.org>
2006-03-22 08:03:15 +0000

    Test for #496 (memory leak in thread locals).

--- cherrypy/test/test_http.py
+++ cherrypy/test/test_http.py
@@ -9,12 +9,18 @@ to the builtin HTTP server (most tests default to 'serverless').
 import test
 test.prefer_parent_path()
 
+import gc
 import httplib
+import threading
 import cherrypy
+from cherrypy import _cphttptools
 
+
+data = object()
 
 class Root:
     def index(self, *args, **kwargs):
+        cherrypy.thread_data.thing = data
         return "Hello world!"
     index.exposed = True
 cherrypy.tree.mount(Root())
@@ -38,6 +44,32 @@ class HTTPTests(helper.CPWebCase):
             c = httplib.HTTPConnection("localhost:%s" % self.PORT)
             c.request("POST", "/")
             self.assertEqual(c.getresponse().status, 411)
+
+
+def get_instances(cls):
+    return [x for x in gc.get_objects() if isinstance(x, cls)]
+
+
+class ReferenceTests(helper.CPWebCase):
+    
+    def test_threadlocal_garbage(self):
+        def getpage():
+            self.getPage('/')
+            self.assertBody("Hello world!")
+        
+        ts = []
+        for _ in range(100):
+            t = threading.Thread(target=getpage)
+            ts.append(t)
+            t.start()
+        
+        for t in ts:
+            t.join()
+        
+        self.assertEqual(gc.collect(), 0)
+        self.assertEqual(len(get_instances(_cphttptools.Request)), 0)
+        self.assertEqual(len(get_instances(_cphttptools.Response)), 0)
+        self.assertEqual(len(gc.get_referrers(data)), 1)
 
 
 if __name__ == '__main__':
