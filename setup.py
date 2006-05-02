4ac872417942a04413104b65f1a8a63093a9432a
Robert Brewer <fumanchu@aminus.org>
2006-05-02 21:26:26 +0000

    Fix for #513 and #515 (setup.py updates).

--- setup.py
+++ setup.py
@@ -18,12 +18,12 @@ required_python_version = '2.3'
 # arguments for the setup command
 ###############################################################################
 name = "CherryPy"
-version = "2.2.0"
+version = "3.0.0alpha"
 desc = "Object-Oriented web development framework"
 long_desc = "CherryPy is a pythonic, object-oriented web development framework"
 classifiers=[
-    "Development Status :: 5 - Production/Stable",
-    #"Development Status :: 4 - Beta",
+    #"Development Status :: 5 - Production/Stable",
+    "Development Status :: 3 - Alpha",
     "Intended Audience :: Developers",
     "License :: Freely Distributable",
     "Programming Language :: Python",
@@ -35,8 +35,8 @@ author_email="team@cherrypy.org"
 url="http://www.cherrypy.org"
 cp_license="BSD"
 packages=[
-    "cherrypy", "cherrypy.lib", "cherrypy.lib.filter",
-    "cherrypy.tutorial", "cherrypy.test", "cherrypy.filters",
+    "cherrypy", "cherrypy.lib",
+    "cherrypy.tutorial", "cherrypy.test",
 ]
 download_url="http://sourceforge.net/project/showfiles.php?group_id=56099"
 data_files=[
@@ -84,47 +84,6 @@ def main():
         data_files=data_files,
     )
 
-    # the code in the following "if" block handles situations where the
-    # user has installed over an older release of 2.1 with a conflicting
-    # version of the sessionfilter
-    if 'install_lib' in dist.command_obj:
-        
-        # get the installation directory (is there a better way to do this?)
-        install_dir = dist.command_obj['install_lib'].install_dir
-
-        # make sure we have an absolute install path
-        install_dir = os.path.join(os.path.abspath('/'), install_dir)
-        
-        old_session_filter_path = os.path.join(
-            install_dir, 'cherrypy', 'lib', 'filter', 'sessionfilter')
-        
-        # check for existence of old sessionfilter package dir
-        # and prompt the user if it exists
-        if os.path.exists(old_session_filter_path):
-            handle_old_session_filter(old_session_filter_path)
-
-        
-def handle_old_session_filter(pth):
-    warn_old_session_filter(pth)
-    choice = raw_input('Delete old sessionfilter directory? (yes/no): ')
-    if choice.lower() in ('y', 'yes'):
-        shutil.rmtree(pth)
-        print "Old sessionfilter directory deleted."
-    else:
-        print "You will need to manually need to delete the old sessionfilter directory."
-
-
-def warn_old_session_filter(pth):
-    msg = """
-************************ WARNING *****************************
- Since you have installed over the top of an existing CherryPy
- installation, you must remove the old sessionfilter package
- directory at:
- %s
-************************ WARNING *****************************
-""" % (pth,)
-    print msg
-    
 
 if __name__ == "__main__":
     main()
