12fe491e3a2a657c8a3f2c36ad0c65da3d98f319
Robert Brewer <fumanchu@aminus.org>
2006-05-10 07:37:40 +0000

    New tests for params with virtual_host.

--- cherrypy/test/test_virtualhost_filter.py
+++ cherrypy/test/test_virtualhost_filter.py
@@ -13,7 +13,11 @@ def setup_server():
         def dom4(self):
             return "Under construction"
         dom4.exposed = True
-
+        
+        def method(self, value):
+            return "You sent %s" % repr(value)
+        method.exposed = True
+    
     class VHost:
         def __init__(self, sitename):
             self.sitename = sitename
@@ -21,8 +25,12 @@ def setup_server():
         def index(self):
             return "Welcome to %s" % self.sitename
         index.exposed = True
-
-
+        
+        def vmethod(self, value):
+            return "You sent %s" % repr(value)
+        vmethod.exposed = True
+    
+    
     root = Root()
     root.mydom2 = VHost("Domain 2")
     root.mydom3 = VHost("Domain 3")
@@ -53,6 +61,17 @@ class VirtualHostFilterTest(helper.CPWebCase):
         self.assertBody('Welcome to Domain 3')
         self.getPage("/", [('Host', 'www.mydom4.com')])
         self.assertBody('Under construction')
+        
+        # Test GET, POST, and positional params
+        self.getPage("/method?value=root")
+        self.assertBody("You sent 'root'")
+        self.getPage("/vmethod?value=dom2+GET", [('Host', 'www.mydom2.com')])
+        self.assertBody("You sent 'dom2 GET'")
+        self.getPage("/vmethod", [('Host', 'www.mydom3.com')], method="POST",
+                     body="value=dom3+POST")
+        self.assertBody("You sent 'dom3 POST'")
+        self.getPage("/vmethod/pos", [('Host', 'www.mydom3.com')])
+        self.assertBody("You sent 'pos'")
 
 
 if __name__ == "__main__":
