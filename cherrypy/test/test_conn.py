f876c5cc8fd56843c7badbb73f463cc91ae1e321
Robert Brewer <fumanchu@aminus.org>
2008-01-20 04:04:05 +0000

    Timeout fix to test_conn.

--- cherrypy/test/test_conn.py
+++ cherrypy/test/test_conn.py
@@ -214,6 +214,9 @@ class ConnectionTests(helper.CPWebCase):
             # Make an initial request
             self.persistent = True
             conn = self.HTTP_CONN
+            # Make the socket nonblocking so it can timeout
+            conn.connect()
+            conn.sock.settimeout(0.1)
             conn.putrequest("GET", "/", skip_host=True)
             conn.putheader("Host", self.HOST)
             conn.endheaders()
