0f65be530c28121077902615623fddb028e1f776
Sylvain Hellegouarch <sh@defuze.org>
2008-02-18 19:18:55 +0000

    Test and fix for #706
    --HG--
    branch : cherrypy-3.0.x

--- cherrypy/_cpdispatch.py
+++ cherrypy/_cpdispatch.py
@@ -320,7 +320,7 @@ class RoutesDispatcher(object):
             
             action = result.get('action', None)
             if action is not None:
-                handler = getattr(controller, action)
+                handler = getattr(controller, action, None)
                 # Get config from the handler 
                 if hasattr(handler, "_cp_config"): 
                     merge(handler._cp_config)
