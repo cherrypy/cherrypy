17cee2a1e81e84902b7bfd68ef92fd94f8e11207
visteya <none@none>
2008-09-28 07:21:29 +0000

    Change lib.static.serve_file() to use a file generator for requests with Ranges, rather than read entire range into memory.  Closes #859.

--- cherrypy/lib/__init__.py
+++ cherrypy/lib/__init__.py
@@ -143,4 +143,16 @@ def file_generator(input, chunkSize=65536):
     input.close()
 
 
+def file_generator_limited(fileobj, count, chunk_size=65536):
+    """Yield the given file object in chunks, stopping after `count`
+    bytes has been emitted.  Default chunk size is 64kB. (Core)
+    """
+    remaining = count
+    while remaining > 0:
+        chunk = fileobj.read(min(chunk_size, remaining))
+        chunklen = len(chunk)
+        if chunklen == 0:
+            return
+        remaining -= chunklen
+        yield chunk
 
