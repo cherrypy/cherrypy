57a026ed969ab541306d963d8320c1aae68c1287
jaraco <none@none>
2009-03-31 21:35:39 +0000

    Implemented workaround for #882 - bdist_wininst doesn't install tutorial (and other data files) where expected

--- setup.py
+++ setup.py
@@ -72,6 +72,24 @@ data_files=[
 # end arguments for setup
 ###############################################################################
 
+def fix_data_files(data_files):
+    """
+    bdist_wininst seems to have a bug about where it installs data files.
+    I found a fix the django team used to work around the problem at
+    http://code.djangoproject.com/changeset/8313 .  This function
+    re-implements that solution.
+    Also see http://mail.python.org/pipermail/distutils-sig/2004-August/004134.html
+    for more info.
+    """
+    def fix_dest_path(path):
+        return '\\PURELIB\\%(path)s' % vars()
+    
+    if not 'bdist_wininst' in sys.argv: return
+    
+    data_files[:] = [
+        (fix_dest_path(path), files)
+        for path, files in data_files]
+fix_data_files(data_files)
 
 def main():
     if sys.version < required_python_version:
