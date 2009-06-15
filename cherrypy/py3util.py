ffdf13c8217f3a785fe8768697b3e3da4b6ff9cb
Robert Brewer <fumanchu@aminus.org>
2009-06-15 00:00:12 +0000

    Use builtin sorted, reversed if available.

--- cherrypy/py3util.py
+++ cherrypy/py3util.py
@@ -3,11 +3,17 @@ A simple module that helps unify the code between a python2 and python3 library.
 """
 import sys
 
-def sorted(lst):
-    newlst = list(lst)
-    newlst.sort()
-    return newlst
+try:
+    sorted = sorted
+except NameError:
+    def sorted(lst):
+        newlst = list(lst)
+        newlst.sort()
+        return newlst
 
-def reversed(lst):
-    newlst = list(lst)
-    return iter(newlst[::-1])
+try:
+    reversed = reversed
+except NameError:
+    def reversed(lst):
+        newlst = list(lst)
+        return iter(newlst[::-1])
