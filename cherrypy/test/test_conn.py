9701ec07ec892bae137347383dfa1c3b859b4951
Robert Brewer <fumanchu@aminus.org>
2009-08-05 00:23:34 +0000

    Fix for #951 (Content-Length breaks curl on 100-Continue) for 3.0, 3.1, 3.2 trunk.
    --HG--
    branch : cherrypy-3.0.x

--- cherrypy/test/test_conn.py
+++ cherrypy/test/test_conn.py
@@ -331,8 +331,10 @@ class ConnectionTests(helper.CPWebCase):
         version, status, reason = response._read_status()
         self.assertEqual(status, 100)
         while True:
-            skip = response.fp.readline().strip()
-            if not skip:
+            line = response.fp.readline().strip()
+            if line:
+                self.fail("100 Continue should not output any headers. Got %r" % line)
+            else:
                 break
         
         # ...send the body
