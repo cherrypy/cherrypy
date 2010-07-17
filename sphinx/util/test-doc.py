b5a968e748e1f989726d652b62c25316951cec95
jaraco <none@none>
2010-07-17 17:27:24 +0000

    Adding script I used to print the documentation for a particular class

---
+++ sphinx/util/test-doc.py
@@ -0,0 +1,22 @@
+from __future__ import print_function
+import sys
+import inspect
+import docutils.utils
+import docutils.parsers.rst
+from StringIO import StringIO
+
+def print_with_line_numbers(block):
+	stream = StringIO(block)
+	for number, line in enumerate(stream):
+		number += 1
+		print(number, line.rstrip())
+
+target_class_spec = sys.argv[1]
+import cherrypy
+target_class = eval(target_class_spec)
+source = inspect.getdoc(target_class)
+print_with_line_numbers(source)
+parser = docutils.parsers.rst.Parser()
+settings = None ## ?
+document = docutils.utils.new_document(source, settings)
+parser.parse(source, document)
