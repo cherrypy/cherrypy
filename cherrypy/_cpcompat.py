1d031219a0113130cc114e4db6247d9d27302aed
Jason R. Coombs <jaraco@jaraco.com>
2012-03-25 00:38:31 -0400

    Fixed issue with nto? not failing on unexpected types.
    --HG--
    branch : cherrypy-3.2.x

--- cherrypy/_cpcompat.py
+++ cherrypy/_cpcompat.py
@@ -27,10 +27,12 @@ if sys.version_info >= (3, 0):
     basestring = (bytes, str)
     def ntob(n, encoding='ISO-8859-1'):
         """Return the given native string as a byte string in the given encoding."""
+        assert_native(n)
         # In Python 3, the native string type is unicode
         return n.encode(encoding)
     def ntou(n, encoding='ISO-8859-1'):
         """Return the given native string as a unicode string with the given encoding."""
+        assert_native(n)
         # In Python 3, the native string type is unicode
         return n
     def tonative(n, encoding='ISO-8859-1'):
@@ -52,12 +54,14 @@ else:
     basestring = basestring
     def ntob(n, encoding='ISO-8859-1'):
         """Return the given native string as a byte string in the given encoding."""
+        assert_native(n)
         # In Python 2, the native string type is bytes. Assume it's already
         # in the given encoding, which for ISO-8859-1 is almost always what
         # was intended.
         return n
     def ntou(n, encoding='ISO-8859-1'):
         """Return the given native string as a unicode string with the given encoding."""
+        assert_native(n)
         # In Python 2, the native string type is bytes.
         # First, check for the special encoding 'escape'. The test suite uses this
         # to signal that it wants to pass a string with embedded \uXXXX escapes,
@@ -85,6 +89,10 @@ else:
         from StringIO import StringIO
     # bytes:
     BytesIO = StringIO
+
+def assert_native(n):
+    if not isinstance(n, nativestr):
+        raise TypeError("n must be a native str (got %s)" % type(n).__name__)
 
 try:
     set = set
