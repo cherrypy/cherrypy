c5137f35bd0361a8a55c2a1174360fb8a7844f15
Jason R. Coombs <jaraco@jaraco.com>
2012-10-24 16:29:59 -0400

    Adding release script
    --HG--
    branch : cherrypy-3.2.x

---
+++ release.py
@@ -0,0 +1,7 @@
+"""
+This script will walk a developer through the process of cutting a release.
+
+To cut a release, simply invoke this script at the changeset to be released.
+"""
+
+raise NotImplementedError("This script is stubbed.")
