d9cbc204e9a2c75b145b998603bc18b33f02b09a
Jason R. Coombs <jaraco@jaraco.com>
2012-10-24 16:36:02 -0400

    Draft of the release process
    --HG--
    branch : cherrypy-3.2.x

--- release.py
+++ release.py
@@ -4,4 +4,35 @@ This script will walk a developer through the process of cutting a release.
 To cut a release, simply invoke this script at the changeset to be released.
 """
 
-raise NotImplementedError("This script is stubbed.")
+def check_status():
+	"""
+	Make sure there aren't outstanding changesets that are unpushed, maybe
+	run the tests or ask the user if the tests are passing.
+	"""
+	raise NotImplementedError()
+
+def bump_versions():
+	"""
+	Bump the versions in each of the places where it appears and commit.
+	"""
+	places = ('setup.py (twice)', 'cherrypy/__init__.py',
+		'cherrypy/wsgiserver/wsgiserver\d')
+	raise NotImplementedError()
+
+def tag_release():
+	"""
+	Tag the release.
+	"""
+	raise NotImplementedError()
+
+def build_and_upload():
+	raise NotImplementedError()
+
+def main():
+	check_status()
+	bump_versions()
+	tag_release()
+	build_and_upload()
+
+if __name__ == '__main__':
+	main()
