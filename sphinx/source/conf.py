bc2e28596930f100e6cd1c8c8e79fee0179dc719
Sylvain Hellegouarch <sh@defuze.org>
2013-11-11 22:01:54 +0100

    let's see if this makes readthedocs happy

--- sphinx/source/conf.py
+++ sphinx/source/conf.py
@@ -211,3 +211,36 @@ latex_documents = [
 
 # If false, no module index is generated.
 #latex_use_modindex = True
+
+import os
+
+on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
+if on_rtd:
+    # so that ReadTheDocs can generate the docs properly
+    # even the PDF version. Since ReadTheDocs runs on Linux,
+    # it can't import pywin32. See:
+    # http://read-the-docs.readthedocs.org/en/latest/faq.html#i-get-import-errors-on-libraries-that-depend-on-c-modules
+    import sys
+    
+    class Mock(object):
+        def __init__(self, *args, **kwargs):
+            pass
+        
+        def __call__(self, *args, **kwargs):
+            return Mock()
+        
+        @classmethod
+        def __getattr__(cls, name):
+            if name in ('__file__', '__path__'):
+                return '/dev/null'
+            elif name[0] == name[0].upper():
+                mockType = type(name, (), {})
+                mockType.__module__ = __name__
+                return mockType
+            else:
+                return Mock()
+
+    MOCK_MODULES = ['win32api', 'win32con', 'win32event',
+                    'win32service', 'win32serviceutil']
+    for mod_name in MOCK_MODULES:
+        sys.modules[mod_name] = Mock()
