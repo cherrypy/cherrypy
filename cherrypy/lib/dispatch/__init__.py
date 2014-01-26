7ff961f81aabaae2477233839e6b834ac490b169
Gustavo Picon <tabo@tabo.pe>
2014-01-25 20:01:21 -0500

    Moved lib/dispatch.py to lib/dispatch/ package
    --HG--
    branch : cp4

---
+++ cherrypy/lib/dispatch/__init__.py
@@ -0,0 +1,6 @@
+# For compatibility. Consider removing in CP4 final.
+from cherrypy.lib.dispatch.base import (Dispatcher,
+                                        MethodDispatcher,
+                                        RoutesDispatcher,
+                                        VirtualHost,
+                                        XMLRPCDispatcher)
