bda4670eee51397e0daf305fbded37a70e161dba
Gustavo Picon <tabo@tabo.pe>
2014-01-27 00:56:49 -0500

    Moved docstrings to a better place
    --HG--
    branch : cp4

--- cherrypy/lib/dispatch/__init__.py
+++ cherrypy/lib/dispatch/__init__.py
@@ -1,3 +1,14 @@
+"""CherryPy dispatchers.
+
+A 'dispatcher' is the object which looks up the 'page handler' callable
+and collects config for the current request based on the path_info, other
+request attributes, and the application architecture. The core calls the
+dispatcher as early as possible, passing it a 'path_info' argument.
+
+The default dispatcher discovers the page handler by matching path_info
+to a hierarchical arrangement of objects, starting at request.app.root.
+"""
+
 # For compatibility. Consider removing in CP4 final.
 from cherrypy.lib.dispatch.method import MethodDispatcher
 from cherrypy.lib.dispatch.object import Dispatcher
