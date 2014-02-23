e48b7157556830b7489b06edf9c36cce4cb38168
Jason R. Coombs <jaraco@jaraco.com>
2014-02-23 12:12:36 -0500

    Correct declaration of dist commands
    --HG--
    branch : cherrypy-3.2.x

--- release.py
+++ release.py
@@ -82,9 +82,7 @@ def tag_release():
 	"""
 	subprocess.check_call(['hg', 'tag', NEXT_VERSION])
 
-dist_commands = [
-	['sdist', 'bdist_wininst', 'bdist_wheel'],
-]
+dist_commands = ['sdist', 'bdist_wininst', 'bdist_wheel']
 
 def build():
 	if os.path.isfile('MANIFEST'):
