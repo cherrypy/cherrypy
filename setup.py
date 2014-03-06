e816d091b24a4e2f38f77273926362d788871ea5
Joseph Tate <jtate@dragonstrider.com>
2014-03-06 16:22:46 -0500

    Bump version

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ class cherrypy_build_py(build_py):
 # arguments for the setup command
 ###############################################################################
 name = "CherryPy"
-version = "3.2.5"
+version = "3.2.6"
 desc = "Object-Oriented HTTP framework"
 long_desc = "CherryPy is a pythonic, object-oriented HTTP framework"
 classifiers = [
