59c79130500b336bc66479984ef743f422c4de10
Jason R. Coombs <jaraco@jaraco.com>
2014-09-14 00:06:44 -0400

    Bumped to 3.6.1 in preparation for next release.

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ class cherrypy_build_py(build_py):
 # arguments for the setup command
 ###############################################################################
 name = "CherryPy"
-version = "3.6.0"
+version = "3.6.1"
 desc = "Object-Oriented HTTP framework"
 long_desc = "CherryPy is a pythonic, object-oriented HTTP framework"
 classifiers = [
