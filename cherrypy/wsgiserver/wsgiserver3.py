2ad0cb2e211f3ca9ae81b061dd9fa90ed0268f80
Florian Bruhin <me@the-compiler.org>
2015-12-18 18:00:24 +0100

    wsgiserver: Ignore EPROTOTYPE on OS X.
    Due to what's probably an OS X kernel bug, it can return EPROTOTYPE instead of
    EPIPE in some situations:
    
    http://erickt.github.io/blog/2014/11/19/adventures-in-debugging-a-potential-osx-kernel-bug/
    
    Fixes #1392.

--- cherrypy/wsgiserver/wsgiserver3.py
+++ cherrypy/wsgiserver/wsgiserver3.py
@@ -170,6 +170,8 @@ socket_errors_to_ignore = plat_specific_errors(
 )
 socket_errors_to_ignore.append("timed out")
 socket_errors_to_ignore.append("The read operation timed out")
+if sys.platform == 'darwin':
+    socket_errors_to_ignore.append(plat_specific_errors("EPROTOTYPE"))
 
 socket_errors_nonblocking = plat_specific_errors(
     'EAGAIN', 'EWOULDBLOCK', 'WSAEWOULDBLOCK')
