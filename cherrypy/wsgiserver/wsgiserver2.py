be02256a81a53fd54c6a9cc9e2655bc55ef9e1d1
Jason R. Coombs <jaraco@jaraco.com>
2015-12-19 23:55:04 -0500

    Bumped to 4.0.1 in preparation for next release.

--- cherrypy/wsgiserver/wsgiserver2.py
+++ cherrypy/wsgiserver/wsgiserver2.py
@@ -1762,7 +1762,7 @@ class HTTPServer(object):
     timeout = 10
     """The timeout in seconds for accepted connections (default 10)."""
 
-    version = "CherryPy/4.0.0"
+    version = "CherryPy/4.0.1"
     """A version string for the HTTPServer."""
 
     software = None
