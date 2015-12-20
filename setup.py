c2987bbab8eea7a311e2a207aed5c0cf7620a46b
Jason R. Coombs <jaraco@jaraco.com>
2015-12-19 22:55:07 -0500

    Bump minimum requirement to Python 2.6

--- setup.py
+++ setup.py
@@ -49,13 +49,12 @@ classifiers = [
     "License :: OSI Approved :: BSD License",
     "Programming Language :: Python",
     "Programming Language :: Python :: 2",
-    "Programming Language :: Python :: 2.3",
-    "Programming Language :: Python :: 2.4",
-    "Programming Language :: Python :: 2.5",
     "Programming Language :: Python :: 2.6",
     "Programming Language :: Python :: 2.7",
     "Programming Language :: Python :: 3",
     "Programming Language :: Python :: 3.3",
+    "Programming Language :: Python :: 3.4",
+    "Programming Language :: Python :: 3.5",
     "Programming Language :: Python :: Implementation",
     "Programming Language :: Python :: Implementation :: CPython",
     "Programming Language :: Python :: Implementation :: Jython",
@@ -113,7 +112,7 @@ cmd_class = dict(
 if sys.version_info >= (3, 0):
     required_python_version = '3.0'
 else:
-    required_python_version = '2.3'
+    required_python_version = '2.6'
 
 ###############################################################################
 # end arguments for setup
