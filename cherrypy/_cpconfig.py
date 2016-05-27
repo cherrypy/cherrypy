e9113058a11985f72a51a1202f8b8ac1d29e43d0
Jason R. Coombs <jaraco@jaraco.com>
2016-05-27 11:40:33 -0400

    Extract variable to avoid awkward indent

--- cherrypy/_cpconfig.py
+++ cherrypy/_cpconfig.py
@@ -309,8 +309,8 @@ def _tree_namespace_handler(k, v):
     if isinstance(v, dict):
         for script_name, app in v.items():
             cherrypy.tree.graft(app, script_name)
-            cherrypy.engine.log("Mounted: %s on %s" %
-                                (app, script_name or "/"))
+            msg = "Mounted: %s on %s" % (app, script_name or "/")
+            cherrypy.engine.log(msg)
     else:
         cherrypy.tree.graft(v, v.script_name)
         cherrypy.engine.log("Mounted: %s on %s" % (v, v.script_name or "/"))
