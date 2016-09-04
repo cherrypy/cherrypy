d99cd42246f791403ec8f1a8326765b114e2c8bf
Jason R. Coombs <jaraco@jaraco.com>
2016-09-04 14:26:19 -0400

    Create 'try_import' to handle the import dance

--- docs/conf.py
+++ docs/conf.py
@@ -16,11 +16,13 @@ import sys
 import os
 import importlib
 
-try:
-    import sphinx_rtd_theme
-    WITH_RTD_THEME = True
-except ImportError:
-    WITH_RTD_THEME = False
+def try_import(mod_name):
+    try:
+        return importlib.import_module(mod_name)
+    except ImportError:
+        pass
+
+sphinx_rtd_theme = try_import('sphinx_rtd_theme')
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
@@ -111,7 +113,7 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  Major themes that come with
 # Sphinx are currently 'default' and 'sphinxdoc'.
-html_theme = "sphinx_rtd_theme" if WITH_RTD_THEME else 'default'
+html_theme = getattr(sphinx_rtd_theme, '__name__', 'default')
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -133,7 +135,7 @@ html_theme = "sphinx_rtd_theme" if WITH_RTD_THEME else 'default'
 
 # Add any paths that contain custom themes here, relative to this directory.
 #html_theme_path = []
-if WITH_RTD_THEME:
+if sphinx_rtd_theme:
     html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # The name for this set of Sphinx documents.  If None, it defaults to
@@ -245,14 +247,7 @@ def mock_pywin32():
     See:
     http://read-the-docs.readthedocs.org/en/latest/faq.html#i-get-import-errors-on-libraries-that-depend-on-c-modules
     """
-    try:
-        win32api = importlib.import_module('win32api')
-    except ImportError:
-        pass
-
-    has_pywin32 = 'win32api' in locals()
-
-    if not has_pywin32:
+    if try_import('win32api'):
         return
 
     class Mock(object):
