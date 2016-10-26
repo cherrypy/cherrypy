63ad72866a8055e7bdf850457bbd6ed7401c4ae5
Paulo Henrique Rodrigues Pinheiro <paulohrpinheiro@gmail.com>
2016-10-26 14:20:19 -0200

    Add tests for _escape from _cpcompat

--- cherrypy/test/test_compat.py
+++ cherrypy/test/test_compat.py
@@ -21,3 +21,23 @@ class StringTester(unittest.TestCase):
         if six.PY3:
             raise nose.SkipTest('Only useful on Python 2')
         self.assertRaises(Exception, compat.ntob, 'fight')
+
+
+class EscapeTester(unittest.TestCase):
+
+    def test_escape_module(self):
+        """test_escape_module - Verify the imported module to _escape function
+
+        See #1330.
+        """
+        print("_escape:", compat._escape.__module__)
+        if six.PY2: # See discussion on https://docs.python.org/3/howto/pyporting.html
+            self.assertEqual('cgi', compat._escape.__module__)
+        else:
+            self.assertEqual('html', compat._escape.__module__)
+
+    def test_escape_quote(self):
+        """test_escape_quote - Verify the output for &<>" chars
+        """
+        self.assertEqual('xx&amp;&lt;&gt;"aa', compat._escape('xx&<>"aa'))
+
