aa0a6a7ad3028d2cf4670d8411b22436dcade21a
Paulo Henrique Rodrigues Pinheiro <paulohrpinheiro@gmail.com>
2016-10-31 13:05:57 -0200

    Fix style conform travis RUN_LINST output

--- cherrypy/test/test_compat.py
+++ cherrypy/test/test_compat.py
@@ -24,11 +24,8 @@ class StringTester(unittest.TestCase):
 
 
 class EscapeTester(unittest.TestCase):
-    """Class to test escape_html function from _cpcompat.
-    """
+    """Class to test escape_html function from _cpcompat."""
 
     def test_escape_quote(self):
-        """test_escape_quote - Verify the output for &<>"' chars.
-        """
-
+        """test_escape_quote - Verify the output for &<>"' chars."""
         self.assertEqual("""xx&amp;&lt;&gt;"aa'""", compat.escape_html("""xx&<>"aa'"""))
