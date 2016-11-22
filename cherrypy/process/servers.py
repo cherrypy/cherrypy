f2580c3c22e543155f3176fed93d0d3e51d1557e
Swapnil Gupta <500swapnil@github.com>
2016-11-22 18:10:34 +0530

    Removed bugs

--- cherrypy/process/servers.py
+++ cherrypy/process/servers.py
@@ -185,7 +185,7 @@ class ServerAdapter(object):
         if not self.httpserver:
             return ''
         host, port = self.bind_addr
-        if getattr(self.httpserver, 'ssl_adapter', None):
+        if getattr(self, 'ssl_certificate', None):
             scheme = 'https'
             if port != 443:
                 host += ':%s' % port
