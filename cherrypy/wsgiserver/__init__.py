9f87f8775d4ce435c6221e1046014b4f879df7b6
LenovoCindywang <wangpeng2284@sina.com>
2016-11-23 14:03:10 +0800

    Update __init__.py

--- cherrypy/wsgiserver/__init__.py
+++ cherrypy/wsgiserver/__init__.py
@@ -1771,7 +1771,7 @@ class SSLAdapter(object):
           socket file object``
     """
 
-    def __init__(self, certificate, private_key, ciphers, certificate_chain=None):
+    def __init__(self, certificate, private_key, certificate_chain=None, ciphers):
         self.certificate = certificate
         self.private_key = private_key
         self.ciphers = ciphers
