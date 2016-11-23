0e11205a19bb3e28a45110efdedfcffe2189ce0a
LenovoCindywang <wangpeng2284@sina.com>
2016-11-23 13:56:04 +0800

    Update _cpnative_server.py

--- cherrypy/_cpnative_server.py
+++ cherrypy/_cpnative_server.py
@@ -144,13 +144,13 @@ class CPHTTPServer(wsgiserver.HTTPServer):
             self.ssl_adapter = adapter_class(
                 self.server_adapter.ssl_certificate,
                 self.server_adapter.ssl_private_key,
-                self.server_adapter.ssl_ciphers,
-                self.server_adapter.ssl_certificate_chain)
+                self.server_adapter.ssl_certificate_chain,
+                self.server_adapter.ssl_ciphers)
             self.ssl_adapter.context = self.server_adapter.ssl_context
         elif self.server_adapter.ssl_certificate:
             adapter_class = wsgiserver.get_ssl_adapter_class(ssl_module)
             self.ssl_adapter = adapter_class(
                 self.server_adapter.ssl_certificate,
                 self.server_adapter.ssl_private_key,
-                self.server_adapter.ssl_ciphers,
-                self.server_adapter.ssl_certificate_chain)
+                self.server_adapter.ssl_certificate_chain,
+                self.server_adapter.ssl_ciphers)
