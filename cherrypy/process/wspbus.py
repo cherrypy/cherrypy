cherrypy/cherrypy

commit ddad3873ec1caa94dfa2f6f18bfc700bea690c44

Don't restart frozen app with interpreter args
With this commit I'm trying to get rid of issue #1526: we should not
add interpreter arguments while re-spawning the app in ``frozen`` mode

cherrypy/process/wspbus.py
@@ -393,7 +393,7 @@ class Bus(object):
             args = self._get_true_argv()
         except NotImplementedError:
             """It's probably win32 or GAE"""
-            args = [sys.executable] + _args_from_interpreter_flags() + sys.argv
+            args = [sys.executable] + self._get_interpreter_argv() + sys.argv
 
         self.log('Re-spawning %s' % ' '.join(args))
 
@@ -410,6 +410,24 @@ class Bus(object):
             if self.max_cloexec_files:
                 self._set_cloexec()
             os.execv(sys.executable, args)
+
+    @staticmethod
+    def _get_interpreter_argv():
+        """Retrieve current Python interpreter's arguments
+
+        Returns empty tuple in case of frozen mode, uses built-in arguments
+        reproduction function otherwise.
+
+        Frozen mode is possible for the app has been packaged into a binary
+        executable using py2exe. In this case the interpreter's arguments are
+        already built-in into that executable.
+
+        :seealso: https://github.com/cherrypy/cherrypy/issues/1526
+        Ref: https://pythonhosted.org/PyInstaller/runtime-information.html
+        """
+        return (()
+                if hasattr(sys, 'frozen', False)
+                else _args_from_interpreter_flags())
 
     @staticmethod
     def _get_true_argv():

commit 3dcb668323a33b9d90652d1c7899412a8ac05838

Fix a few bugs introduced in ddad387
Basically, we cannot add ``tuple`` to ``list``. Also, using
``getattr`` instead of ``hasattr`` is more reliable.

Ref: #1526

cherrypy/process/wspbus.py
@@ -425,8 +425,8 @@ class Bus(object):
         :seealso: https://github.com/cherrypy/cherrypy/issues/1526
         Ref: https://pythonhosted.org/PyInstaller/runtime-information.html
         """
-        return (()
-                if hasattr(sys, 'frozen', False)
+        return ([]
+                if getattr(sys, 'frozen', False)
                 else _args_from_interpreter_flags())
 
     @staticmethod