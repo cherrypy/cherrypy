cfde0dace2cdac2ab9b0ecaafbd86c5e3281fc9e
Jason R. Coombs <jaraco@jaraco.com>
2017-01-06 06:25:13 -0500

    Mark test as xfail until it can be corrected. Ref #1544.

--- cherrypy/test/test_static.py
+++ cherrypy/test/test_static.py
@@ -370,6 +370,11 @@ class StaticTest(helper.CPWebCase):
         finally:
             os.remove(filepath)
 
+    py27_on_windows = (
+        platform.system() == 'Windows' and
+        sys.version_info < (3,)
+    )
+    @pytest.mark.xfail(py27_on_windows, reason="#1544")
     def test_unicode(self):
         with self.unicode_file():
             url = ntou('/static/Слава Україні.html', 'utf-8')
