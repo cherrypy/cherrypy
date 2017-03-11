661f662e764c5e1f6b64a22bf2f10848adc8ec9b
Sviatoslav Sydorenko <wk@sydorenko.org.ua>
2017-03-11 13:51:44 +0200

    Don't install graphviz>=0.6 under Python 2.6
    Fixes failing build since graphviz dropped support for Python 2.6
    Ref: https://github.com/xflr6/graphviz/pull/32

--- setup.py
+++ setup.py
@@ -89,6 +89,8 @@ extras_require = {
 
     # http://docs.cherrypy.org/en/latest/advanced.html?highlight=windows#windows-console-events
     ':sys_platform == "win32"': ['pypiwin32'],
+
+    ':python_version == "2.6"': ['graphviz<0.6'],  # v0.6 has dropped support for 2.6
 }
 """Feature flags end-users can use in dependencies"""
 
