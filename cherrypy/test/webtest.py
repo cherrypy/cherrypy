41ea50ae4e9c54f14382a5d0ccd1ba5bcb5c87a2
Luis Presuel <luis.presuel@aluxoft.com>
2018-04-06 10:38:31 -0500

    Removed no longer existent imports from cheroot webtest. cheroot:issue #42

--- cherrypy/test/webtest.py
+++ cherrypy/test/webtest.py
@@ -2,8 +2,7 @@
 import warnings
 
 from cheroot.test.webtest import (  # noqa
-    interface, TerseTestResult, TerseTestRunner,
-    ReloadingTestLoader, WebCase, cleanHeaders, shb, openURL,
+    interface, WebCase, cleanHeaders, shb, openURL,
     ServerError, server_error,
 )
 
