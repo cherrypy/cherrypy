8583123385fcfb9aabd2a74fdaf4e5fb45ae3df5
Sviatoslav Sydorenko <wk@sydorenko.org.ua>
2018-04-22 05:15:30 +0200

    Drop unneeded to_native_or_none

--- cherrypy/lib/auth_digest.py
+++ cherrypy/lib/auth_digest.py
@@ -143,13 +143,10 @@ def H(s):
 def _try_decode_map_values(param_map, charset):
     global FALLBACK_CHARSET
 
-    def to_native_or_none(v, enc):
-        return tonative(v, enc) if v is not None else v
-
     for enc in (charset, FALLBACK_CHARSET):
         try:
             return {
-                k: to_native_or_none(v, enc)
+                k: tonative(v, enc)
                 for k, v in param_map.items()
             }
         except ValueError as ve:
