a8abae89c6dd41aefa9dcf4e815e454fc1dbe29c
Sviatoslav Sydorenko <wk@sydorenko.org.ua>
2018-08-15 10:57:29 +0200

    Advertise Python 3.7 support via metadata

--- setup.py
+++ setup.py
@@ -30,6 +30,7 @@ params = dict(
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: Implementation',
         'Programming Language :: Python :: Implementation :: CPython',
         'Programming Language :: Python :: Implementation :: Jython',
