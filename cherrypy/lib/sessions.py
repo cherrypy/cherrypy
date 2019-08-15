a9d67b4ff47f5a03cd48e8b7521ae03cb4307bc6
eric <eric@hellman.net>
2019-08-15 15:02:11 -0400

    catch RuntimeError
    So the cleanup thead doesn't die

--- cherrypy/lib/sessions.py
+++ cherrypy/lib/sessions.py
@@ -404,18 +404,28 @@ class RamSession(Session):
         """Clean up expired sessions."""
 
         now = self.now()
-        for _id, (data, expiration_time) in list(self.cache.items()):
-            if expiration_time <= now:
-                try:
-                    del self.cache[_id]
-                except KeyError:
-                    pass
-                try:
-                    if self.locks[_id].acquire(blocking=False):
-                        lock = self.locks.pop(_id)
-                        lock.release()
-                except KeyError:
-                    pass
+        try:
+            for _id, (data, expiration_time) in list(self.cache.items()):
+                if expiration_time <= now:
+                    try:
+                        del self.cache[_id]
+                    except KeyError:
+                        pass
+                    try:
+                        if self.locks[_id].acquire(blocking=False):
+                            lock = self.locks.pop(_id)
+                            lock.release()
+                    except KeyError:
+                        pass
+
+        except RuntimeError:
+            """Under heavy load, list(self.cache.items()) will occasionally raise this
+            error for large session caches with message
+            "dictionary changed size during iteration"
+            Better to pause the cleanup than to let the cleanup thread die.
+            """
+
+            return
 
         # added to remove obsolete lock objects
         for _id in list(self.locks):
