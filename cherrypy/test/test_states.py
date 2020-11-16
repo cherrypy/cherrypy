7357e09d3838187edbd6211eca91c8392b09a2ea
Christian Clauss <cclauss@me.com>
2020-11-17 00:54:16 +0100

    Update cherrypy/test/test_states.py
    Co-authored-by: Sviatoslav Sydorenko <wk.cvs.github@sydorenko.org.ua>

--- cherrypy/test/test_states.py
+++ cherrypy/test/test_states.py
@@ -161,9 +161,7 @@ class ServerStateTests(helper.CPWebCase):
         self.assertEqual(db_connection.running, False)
         self.assertEqual(len(db_connection.threads), 0)
 
-    @pytest.mark.skipif(os.name == 'nt', 'see pytest-dev/pytest-forked#44')
-    @pytest.mark.xfail(reason='KeyboardInterrupt error #1873')
-    @pytest.mark.forked
+    @pytest.mark.xfail(reason='KeyboardInterrupt #1873', run=(os.name != 'nt'))
     def test_2_KeyboardInterrupt(self):
         # Raise a keyboard interrupt in the HTTP server's main thread.
         # We must start the server in this, the main thread
