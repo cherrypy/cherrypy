7d7fa3a696abf72d29224303a4da4c010d1872b0
Christian Clauss <cclauss@me.com>
2020-11-17 00:27:40 +0100

    skip the skipif()

--- cherrypy/test/test_states.py
+++ cherrypy/test/test_states.py
@@ -161,12 +161,14 @@ class ServerStateTests(helper.CPWebCase):
         self.assertEqual(db_connection.running, False)
         self.assertEqual(len(db_connection.threads), 0)
 
-    @pytest.mark.skipif(os.name == 'nt', 'see pytest-dev/pytest-forked#44')
+    # @pytest.mark.skipif(os.name == 'nt', 'see pytest-dev/pytest-forked#44')
     @pytest.mark.forked
     @pytest.mark.xfail(bool(os.getenv('CI')), reason='KeyboardInterrupt #1873')
     def test_2_KeyboardInterrupt(self):
         # Raise a keyboard interrupt in the HTTP server's main thread.
         # We must start the server in this, the main thread
+        if os.name == 'nt':
+            return  # Workaround for the pytest.mark.skipif() above.
         engine.start()
         cherrypy.server.start()
 
