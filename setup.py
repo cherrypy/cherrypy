3c8db2d91e04be69476e2ff7249116afb2bdba3e
Mike <mpf82@users.noreply.github.com>
2021-11-10 16:57:22 +0100

    added brotli compression support
    brotli compression can be enabled just like gzip compression by setting ``'tools.br.on': True,``
    
    ``tools.br`` may also be combined with ``'tools.gzip`` - the client can then decide which encoding is accepted.
    
    If both ``gzip`` and ``br`` tools are enabled and both ``gzip`` and ``br`` are accepted encodings, ``br`` will be chosen over ``gzip`` - unless ``gzip`` has a higher ``qvalue``

--- setup.py
+++ setup.py
@@ -95,6 +95,7 @@ params = dict(
             'requests_toolbelt',
             'pytest-services>=2',
             'setuptools',
+            'brotli',
         ],
         # Enables memcached session support via `cherrypy[memcached_session]`:
         'memcached_session': ['python-memcached>=1.58'],
