ef34674a8c35bd5806e86c97cc4701f5306bd732
江文 <2059563+jiangwen365@users.noreply.github.com>
2022-04-16 22:35:15 +0800

    Lock file may not exist, ignore this exception.

--- cherrypy/lib/sessions.py
+++ cherrypy/lib/sessions.py
@@ -560,8 +560,11 @@ class FileSession(Session):
         """Release the lock on the currently-loaded session data."""
         self.lock.close()
         self.locked = False
-        if not self._exists():
-            os.unlink(self._get_file_path() + self.LOCK_SUFFIX)
+        if not self._exists():       
+            try:
+                os.unlink(self._get_file_path() + self.LOCK_SUFFIX)
+            except OSError:
+                pass
 
     def clean_up(self):
         """Clean up expired sessions."""
