fe764cb1598cf87bf3826ac4ee9f98bda060278c
Dan Radez <dradez@redhat.com>
2024-01-09 11:27:05 -0500

    🔧 Stop using the `cgi` stdlib module
    the module was deprecatied in py 3.11 and removed in py 3.13
    there are examples of using the email module to resolve this:
    PEP 594
    https://github.com/python-babel/babel/issues/873
    https://stackoverflow.com/questions/69068527/python-3-cgi-parse-header
    
    This method doesn't seem to work for cherrypy.
    The email.message module is trying to create a higher level
    object that is intelligent about the headers it has defined. The
    cgi.parse_header function is a very low level unintelligent function
    that simply parses header content based on structure and does not
    inspect the contents. Because of the intelligence of the email.message
    object cherrypy can't use it in the very generic way that the
    cgi.parse_header function was being used.
    
    Fix #2014

--- cherrypy/lib/httputil.py
+++ cherrypy/lib/httputil.py
@@ -12,7 +12,7 @@ import email.utils
 import re
 import builtins
 from binascii import b2a_base64
-from cgi import parse_header
+from cherrypy._cpcompat import cgi_parse_header as parse_header
 from email.header import decode_header
 from http.server import BaseHTTPRequestHandler
 from urllib.parse import unquote_plus
